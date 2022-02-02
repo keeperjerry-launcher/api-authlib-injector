9cc64a3d0b25b69dac3efab7a6f2bef2141e384f
KeeperJerry <keeperjerry@yandex.ru>
2022-02-02 19:57:07 +0300

    authserver complete
    Закончил с authserver

--- authlib/authserver/signout.php
+++ authlib/authserver/signout.php
@@ -1,5 +1,6 @@
 <?php
     // DOCS: https://wiki.vg/Authentication#Signout
+    // Скрипт готов
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_settings.php';
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_request.php';
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_pdo_mysql.php';
@@ -30,3 +31,51 @@
     $json = json_decode(file_get_contents('php://input'));
     $email = $json->username;
     $password = $json->password;
+
+    if ($email == NULL || $password == NULL)
+    {
+        die(header("status: 403"));
+    }
+
+    try 
+    {
+        $sql_auth = $pdo->prepare("SELECT {$config['sql_id']},{$config['sql_username']},{$config['sql_password']} FROM {$config['sql_db_table']} WHERE {$config['sql_email']} = :email LIMIT 1");
+        $sql_auth->bindValue(':email', $email);
+        $sql_auth->execute();
+        $sql_auth_result = $sql_auth->fetch(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $e)
+    {
+        if ($config['debug']) die('[DEBUG] Выброшено исключение SQL: ' .  $e->getMessage() . PHP_EOL);
+        request_deny(
+            "Internal Server Error", 
+            "Sorry, server is down! We are working on this problem!"
+        );
+    }
+
+    if($sql_auth_result == null)
+    {
+        die(header("status: 403"));
+    }
+
+    if (!checkPass($password, $sql_auth_result[$config['sql_password']]))
+    {
+        die(header("status: 403"));
+    }
+
+    try
+    {
+        $update_access_token = $pdo->prepare("UPDATE {$config['sql_db_table']} SET {$config['sql_access_token']} = NULL , {$config['sql_client_token']} = NULL WHERE {$config['sql_id']} = :id");
+        $update_access_token->bindValue(':id', $sql_auth_result[$config['sql_id']]);
+        $update_access_token->execute();
+    }
+    catch (PDOException $e)
+    {
+        if ($config['debug']) die('[DEBUG] Выброшено исключение SQL: ' . $e->getMessage() . PHP_EOL);
+        request_deny(
+            "Internal Server Error", 
+            "Sorry, server is down! We are working on this problem!"
+        );
+    }
+
+    die(header("status: 204"));
