c721de54706e328600eaeeb07aaba86eb2d4c2f5
KeeperJerry <keeperjerry@yandex.ru>
2022-02-02 22:25:41 +0300

    Ну, вроде готово
    Проверяю работу на последок

--- authlib/sessionserver/hasJoined.php
+++ authlib/sessionserver/hasJoined.php
@@ -1,5 +1,6 @@
 <?php
     // DOCS: https://wiki.vg/Protocol_Encryption#Server
+    // Скрипт готов
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_settings.php';
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_request.php';
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_pdo_mysql.php';
@@ -50,3 +51,34 @@
             "Invalid ServerId."
         );
     }
+
+    $username = filter_input(INPUT_GET,'username',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $serverid = filter_input(INPUT_GET,'serverId',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $ip_server = filter_input(INPUT_GET,'ip',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    try {
+        $select_joined = $pdo->prepare("SELECT {$config['sql_username']},{$config['sql_uuid']},{$config['sql_skin_hash']},{$config['sql_cloak_hash']} FROM {$config['sql_db_table']} WHERE {$config['sql_username']} = :user AND {$config['sql_server_id']} = :serverid LIMIT 1");
+        $select_joined->bindValue(':user', $username);
+        $select_joined->bindValue(':serverid', $serverid); 
+        $select_joined->execute();
+
+        $row = $select_joined->fetch(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $e)
+    {
+        if ($config['debug']) die('[DEBUG] Выброшено исключение SQL: ' . $e->getMessage() . PHP_EOL);
+        request_deny(
+            "Internal Server Error", 
+            "Sorry, server is down! We are working on this problem!"
+        );
+    }
+
+    if($username != $row[$config['sql_username']])
+    {
+        request_deny(
+            "Not Found", 
+            "Invalid username or password."
+        );
+    }
+
+    request_get_session_profile($row[$config['sql_username']], $row[$config['sql_uuid']], $config['server_url_skins'], $row[$config['sql_skin_hash']] , $row[$config['sql_cloak_hash']]);
--- authlib/sessionserver/join.php
+++ authlib/sessionserver/join.php
@@ -1,5 +1,6 @@
 <?php
     // DOCS: https://wiki.vg/Protocol_Encryption#Client
+    // Скрипт готов
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_settings.php';
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_request.php';
     include $_SERVER['DOCUMENT_ROOT'].'/authlib/_functions/api_pdo_mysql.php';
@@ -26,3 +27,83 @@
             "Сервер отказывается обслуживать запрос, потому что объект запроса находится в формате, не поддерживаемом запрошенным ресурсом для запрошенного метода."
         );
     }
+
+    $json = json_decode(file_get_contents('php://input'));
+    $uuid = $json->selectedProfile;
+    $sessionid = $json->accessToken;
+    $serverid = $json->serverId;
+
+    if (!preg_match("/^[a-zA-Z0-9_-]+$/", $uuid) || empty($uuid))
+    {
+        request_deny(
+            "IllegalArgumentException", 
+            "Неверный UUID. Пожалуйста, обратитесь в поддержку!"
+        );
+    }
+
+    if (!preg_match("/^[a-zA-Z0-9:_-]+$/", $sessionid) || empty($sessionid))
+    {
+        request_deny(
+            "IllegalArgumentException", 
+            "Неверный идентификатор сеанса. Пожалуйста, перезайдите в лаунчер!"
+        );
+    }
+
+    if (!preg_match("/^[a-zA-Z0-9_-]+$/", $serverid) || empty($serverid))
+    {
+        request_deny(
+            "IllegalArgumentException", 
+            "Неверный идентификатор сервера. Пожалуйста, перезайдите в лаунчер!"
+        );
+    }
+
+    try
+    {
+        $stmt_select = $pdo->prepare("SELECT {$config['sql_username']},{$config['sql_uuid']} FROM {$config['sql_db_table']} WHERE {$config['sql_access_token']} = :sessionid LIMIT 1");
+        $stmt_select->bindValue(':sessionid', $sessionid);
+        $stmt_select->execute();
+        
+        $row = $stmt_select->fetch(PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $e)
+    {
+        if ($config['debug']) die('[DEBUG] Выброшено исключение SQL: ' . $e->getMessage() . PHP_EOL);
+        request_deny(
+            "Internal Server Error", 
+            "Извините, сервер не работает! Мы работаем над этой проблемой!"
+        );
+    }
+
+    if($row == null || $row[$config['sql_uuid']] != $uuid)
+    {
+        request_deny(
+            "User not Found", 
+            "Пользователь не найден. Пожалуйста, зарегистрируйтесь на сайте или обратитесь в службу поддержки."
+        );
+    }
+
+    try
+    {
+        $stmt_update = $pdo->prepare("UPDATE {$config['sql_db_table']} SET {$config['sql_server_id']} = :serverid WHERE {$config['sql_access_token']} = :sessionid");
+        $stmt_update->bindValue(':sessionid', $sessionid);
+        $stmt_update->bindValue(':serverid', $serverid);
+        $stmt_update->execute();
+    }
+    catch (PDOException $e)
+    {
+        if ($config['debug']) die('[DEBUG] Выброшено исключение SQL: ' . $e->getMessage() . PHP_EOL);
+        request_deny(
+            "Internal Server Error", 
+            "Извините, сервер не работает! Мы работаем над этой проблемой!"
+        );
+    }
+
+    if($stmt_update->rowCount() != 1)
+    {
+        request_deny(
+            "Internal Server Error", 
+            "Сервисная ошибка! Обратитесь в администрацию!"
+        );
+    }
+
+    die(header("status: 204"));
